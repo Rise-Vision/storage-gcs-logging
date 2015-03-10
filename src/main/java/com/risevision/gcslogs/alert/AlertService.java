433ac164dd4b1ddb6edd3701ea0ea212ba9392d3
tejohnso <mail@tyler-johnson.ca>
2015-03-10 00:54:53 -0400

    Use urlfetch

--- src/main/java/com/risevision/gcslogs/alert/AlertService.java
+++ src/main/java/com/risevision/gcslogs/alert/AlertService.java
@@ -4,34 +4,48 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 import com.google.api.client.googleapis.util.Utils;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.*;
-
+import com.google.appengine.api.modules.ModulesServiceFactory;
+import com.google.api.client.http.EmptyContent; 
+import com.google.appengine.api.urlfetch.*;
+import java.net.URL;
 import static com.risevision.gcslogs.BuildConfig.*;
+import com.google.api.client.util.escape.PercentEscaper;
 
 public class AlertService {
   private static final Logger log =
   Logger.getLogger("gcslogs.AlertService");
 
-  private static HttpRequestFactory requestor =
-  Utils.getDefaultTransport().createRequestFactory();
+  private static URLFetchService fetcher =
+  URLFetchServiceFactory.getURLFetchService();
 
-  private static String URL = "https://http-api-dot-logger-dot-rvacore-test.appspot.com/submit?token=gcs-logs&environment=testenvironment&severity=alert&error_message=MESSAGE&error_details=DETAILS&logger_version=1";
+  private static PercentEscaper escaper = new PercentEscaper("", false);
+  private static FetchOptions opts = FetchOptions.Builder.doNotFollowRedirects();
+
+  private static String alertHost =
+  "http-api-dot-logger-dot-rvacore-test.appspot.com";
+
+  private static String URL = "http://" + alertHost + "/submit?token=gcs-logs&environment=testenvironment&severity=alert&error_message=MESSAGE&error_details=DETAILS&logger_version=1";
 
   public static int alert(String msg, String details) {
     if (msg == null) {return 503;}
     if (details == null) {details = "";}
-    HttpResponse resp = null;
+    HTTPResponse resp = null;
 
-    String url = URL.replace("MESSAGE", msg).replace("DETAILS", details);
-    log.info("TESTING");
+    String url = URL.replace
+    ("MESSAGE", escaper.escape(msg)).replace("DETAILS", escaper.escape(details));
+
+    log.info("TESTING " + url);
     try {
-      resp = requestor.buildPostRequest(new GenericUrl(url), null).execute();
+      resp = fetcher.fetch(new HTTPRequest(new URL(url), HTTPMethod.POST, opts));
     } catch (IOException e) {
       log.severe("Error sending alert \"message " + msg + "\".\n" + e.getMessage());
       e.printStackTrace();
     }
-
-    return resp == null ? 503 : resp.getStatusCode();
+    log.info("TESTING " + new String(resp.getContent()));
+    for (HTTPHeader header : resp.getHeaders()) {
+      log.info(header.getName() + ":" + header.getValue());
+    }
+    log.info("TESTING " + resp.getFinalUrl());
+    return resp == null ? 503 : resp.getResponseCode();
   }
 }
--- src/test/java/integration/com/risevision/gcslogs/alert/AlertServiceIT.java
+++ src/test/java/integration/com/risevision/gcslogs/alert/AlertServiceIT.java
@@ -5,10 +5,24 @@ import java.util.logging.Logger;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import com.google.appengine.tools.development.testing.*;
 
 public class AlertServiceIT {
   private static final Logger log =
   Logger.getLogger("gcslogs.AlertServiceIT");
+
+  private final LocalServiceTestHelper helper =
+  new LocalServiceTestHelper(new LocalURLFetchServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
 
   @Test public void itAlerts() {
     assertThat("it alerted", AlertService.alert("Test message", null), is(200));
